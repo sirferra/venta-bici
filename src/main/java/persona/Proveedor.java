69ecd7d11619374db0cf4f4760f84b5595281f8e
Facundo Cuffia <keloguey@outlook.com>
2024-11-16 19:09:50 -0300

    Llenado de atributos y constructores en clases.

--- src/main/java/persona/Proveedor.java
+++ src/main/java/persona/Proveedor.java
@@ -6,8 +6,45 @@ package persona;
 
 /**
  *
- * @author kelog
+ * @author facundo.cuffia
  */
-public class Proveedor {
+public class Proveedor extends Persona {
+    //Atributos
+    private String cuit;
+    private String nombreFantasia;
     
+    //Constructores
+    public Proveedor(){
+        super();
+    }
+
+    public Proveedor(String cuit, String nombreFantasia) {
+        this.cuit = cuit;
+        this.nombreFantasia = nombreFantasia;
+    }
+
+    public Proveedor(String cuit, String nombreFantasia, String nombre, String apellido, int dni, String telefono, String email) {
+        super(nombre, apellido, dni, telefono, email);
+        this.cuit = cuit;
+        this.nombreFantasia = nombreFantasia;
+    }
+    
+    //Getters y Setters
+    public String getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(String cuit) {
+        this.cuit = cuit;
+    }
+
+    public String getNombreFantasia() {
+        return nombreFantasia;
+    }
+
+    public void setNombreFantasia(String nombreFantasia) {
+        this.nombreFantasia = nombreFantasia;
+    }
+    
+      
 }
