408f97dbad5c8b3500f3be08fdced161aed4a2d4
sirferra <sir.leandroferra@gmail.com>
2024-11-17 17:12:48 -0300

    feat: crud categoria, crud marca

--- src/main/java/producto/Marca.java
+++ src/main/java/producto/Marca.java
@@ -39,6 +39,31 @@ public class Marca {
         this.nombre = nombre;
     }
 
+    // crud marca
+
+    public static boolean crearMarca(String nombre) {
+        try {
+            String sql = "INSERT INTO marca (nombre) VALUES (?)";
+            HashMap<Integer, Object> marca = new HashMap<>();
+            marca.put(1, nombre);
+            Conexion.getInstance().executeQueryWithParams(sql, marca);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static List<Marca> getAll() {        
+        try {
+            ResultSet resultados = Conexion.getInstance().executeQuery("SELECT * FROM Marca");
+            return Marca.fromResultSet(resultados);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public static Marca buscarPorId(int marca_id) {
         try {
             String sql = "SELECT * FROM marca WHERE marca_id = ?";
@@ -51,7 +76,48 @@ public class Marca {
             return null;
         }
     }
-    
+
+    public static List<Marca> buscarPorNombre(String nombre) {
+        try {
+            String sql = "SELECT * FROM marca WHERE nombre = ?";
+            HashMap<Integer, Object> marca = new HashMap<>();
+            marca.put(1, nombre);
+            ResultSet resultados = Conexion.getInstance().executeQueryWithParams(sql, marca);
+            return Marca.fromResultSet(resultados);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean modificarMarca() {
+        try {
+            String sql = "UPDATE marca SET marca_id = ?, nombre = ? WHERE marca_id = ?";
+            HashMap<Integer, Object> marca = new HashMap<>();
+            marca.put(1, getId());
+            marca.put(2, getNombre());
+            marca.put(3, getId());
+            Conexion.getInstance().executeQueryWithParams(sql, marca);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean eliminarMarca() {
+        try {
+            String sql = "DELETE FROM marca WHERE marca_id = ?";
+            HashMap<Integer, Object> marca = new HashMap<>();
+            marca.put(1, getId());
+            Conexion.getInstance().executeQueryWithParams(sql, marca);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     public static List<Marca> fromResultSet(ResultSet resultados) {
         try {
             List<Marca> marcas = new java.util.ArrayList<>();
